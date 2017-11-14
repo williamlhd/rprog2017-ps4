eed2ad3ed6fc9f27eb321299315ee1c7b8f2d5c8
Rossi <rossi.a.rafeh@gmail.com>
2017-11-14 16:22:45 +0100

    add gtrends-script (piece of code students can use) and ps4.pdf (question sheet)

---
+++ gtrends-script.R
@@ -0,0 +1,32 @@
+library(gtrendsR)
+library(ggplot2)
+library(tidyverse)
+
+
+bitcoin.trend <- gtrends(c("bitcoin"), gprop = "web", time = "all")[[1]]
+class(bitcoin.trend)
+head(bitcoin.trend)
+
+class(bitcoin.trend$date)
+
+ggplot(data = bitcoin.trend) + geom_line(mapping = aes(x= date, y = hits))
+?gtrends
+
+bitcoin.trend <- bitcoin.trend %>% filter(date >= as.Date("2009-01-01"))
+ggplot(data = bitcoin.trend) + 
+  geom_line(mapping = aes(x= date, y = hits)) +
+  geom_vline(xintercept = as.Date("2017-01-20"), color = "red") #trump election
+  
+
+library(Quandl)
+bitcoin.price <- Quandl("BCHARTS/BITSTAMPUSD")
+bitcoin.price <- bitcoin.price %>% filter(Date %in% bitcoin.trend$date) %>% select(Date, Close) %>% rename(date = Date, price = Close) %>% mutate(price = price*100/max(price))
+
+bitcoin <- left_join(x = bitcoin.trend, y= bitcoin.price, by = "date")
+head(bitcoin)
+
+ggplot(data = bitcoin) + 
+  geom_line(mapping = aes(x= date, y = hits)) +
+  geom_line(mapping = aes(x= date, y = price), color = "gray") + 
+  geom_vline(xintercept = as.Date("2017-01-20"), color = "red")
+
